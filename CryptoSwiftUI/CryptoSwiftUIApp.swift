28a67120a88fe128294395bf33b3e898318289c6
Murathan Karagöz <murathankaragooz@hotmail.com>
2023-08-27 02:38:00 +0300

    Initial Commit

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Murathan karagöz on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
